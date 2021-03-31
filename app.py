ae639bc4d9166a1430a7d870985433c03cac374d
Diego Bernal <diegobernal0905@gmail.com>
2021-03-31 11:55:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask
+import logging
+
 app = Flask(__name__)
+
+logging.basicConfig(filename='/tmp/demo.log', level=logging.DEBUG)
+
+@app.before_request
+def log_request_info():
+    app.logger.debug('Headers: %s', request.headers)
+    app.logger.debug('Body: %s', request.get_data())
 
 @app.route("/")
 def hello():
-    return "PoC by GoDiego (<a href=https://hackerone.com/diegobernal>https://hackerone.com/diegobernal</a>)"
+    return "Hello, World!"
