cf0bf4465fdea8ecb64425d8c64c030cc6a41b73
Diego Bernal <diegobernal0905@gmail.com>
2021-03-31 11:55:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 from flask import Flask
+import logging
+
 app = Flask(__name__)
+
+logging.basicConfig(filename='/tmp/demo.log', level=logging.DEBUG)
+
+@app.before_request
+def log_request_info():
+    app.logger.debug('Headers: %s', request.headers)
+    app.logger.debug('Body: %s', request.get_data())
 
 @app.route("/")
 def hello():
