fab66b6065d51757bbd311fc408688eb710bc0d4
Diego <diegobernal0905@gmail.com>
2021-04-06 17:21:55 +0100

    Update with 0xd0m7

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
 
+@app.route('/.well-known/pki-validation/76E0FFBF162C024AE65B7626B6EC72B8.txt')
+def hi():
+		return """8E5AEBD3CD5EFD586617827D7B7BAC73C5B48B483E10FEB63B276E998B35A046
+comodoca.com
+607d88781192c11"""
+
+
 @app.route("/")
 def hello():
 		poc = """<html>
